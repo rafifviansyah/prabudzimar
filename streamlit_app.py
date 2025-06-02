f4b169d156f62b16c79781b3b4be746bb7a903da
rafifviansyah <rafifviansyah603@gmail.com>
2025-06-02 19:36:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Informatika Prabu Dzimar") 
+st
+st.header("Aplikasi menilai ganjil/genap") 
+angka= st.number_input("70:", value=0, step=1) 
+
+if(angka % 2) == 0:
+  st.write(f"{2,4,6,8,10} adalah jumlah angka genap") 
+else:
+  st.write(f"{1,3,5,7,9} adalah jumlah angka ganjil")
